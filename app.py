e863d264fdf4241123848e7526e826391e79b782
Mario <mariofbarros.fsma@gmail.com>
2026-03-22 18:09:35 -0300

    setting up basic database (SQLite)

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+class MyOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
 
 @app.route('/')
 def index():
     return "Hello, World"
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
